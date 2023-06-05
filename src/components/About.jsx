b533e226b5c27b10084d178fe22a9d54d05eb785
Potism <felixbakattv@gmail.com>
2023-06-05 22:02:05 +0200

    fix: all

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,26 +1,25 @@
-import React from "react";
 
 const About = () => {
   return (
     <div
       name="about"
-      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white "
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="pb-12">
-          <p className="text-4xl font-bold  border-gray-500">
+        <div>
+          <p className="text-4xl font-bold border-gray-500">
             About Me:
           </p>
         </div>
 
-        <p className="text-xl mt-20 overflow-auto">
+        <p className="text-md mt-20">
         I am a software developer based in Italy with a passion for web development.
          I love solving complex problems and turning them into efficient, high-speed, and reliable applications.
         </p>
 
         <br />
 
-        <p className="text-xl overflow-auto">
+        <p className="text-md">
         In my previous roles, I have demonstrated my ability to rapidly develop and design web applications and websites that meet the specific needs of businesses.
          I believe that user-friendly and visually appealing designs are essential for a smooth and seamless user experience. 
          I have had the privilege of working with some of the most reputable brands in the industry,
